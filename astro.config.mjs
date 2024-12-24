49f0f1e14472c1d6c5059704bae0676d188174bd
Saurabh <75117608+Saurabh254@users.noreply.github.com>
2024-12-24 22:07:43 +0530

    fix: fixed ci and formatting (#1)
    * fix: fixed ci and formatting
    
    * fix: fix typo in the workflows folder name
    
    * fix: fix node version in ci

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,16 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from 'astro/config'
 
-import svelte from '@astrojs/svelte';
+import svelte from '@astrojs/svelte'
 
-import tailwind from '@astrojs/tailwind';
+import tailwind from '@astrojs/tailwind'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [svelte({ extensions: ['.svelte'] }), tailwind(
-    {
-      configFile: './tailwind.config.mjs'
-    }
-  )]
-});
+  integrations: [
+    svelte({ extensions: ['.svelte'] }),
+    tailwind({
+      configFile: './tailwind.config.mjs',
+    }),
+  ],
+})
