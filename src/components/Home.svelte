2590c3e91c54f44bcdb4e90fa79178a296c3f4f1
Saurabh <75117608+Saurabh254@users.noreply.github.com>
2024-12-28 23:11:33 +0530

    feat: Added homescreen for lg screen only (#4)

--- src/components/Home.svelte
+++ src/components/Home.svelte
@@ -1,1 +1,54 @@
-<div>my name is saurabh Vishwakarma</div>
+<script lang="ts">
+</script>
+
+<div
+  class="mt-4 w-full h-full flex items-center justify-center mx-8 text-[#676767]"
+>
+  <div class="flex w-[80%]">
+    <!-- info section -->
+    <div class=" flex flex-col justify-center">
+      <div
+        class="bg-gray-200 relative px-4 py-2 font-bold text-[#676767] justify-center rounded-lg w-fit"
+      >
+        It's me
+        <svg
+          class="absolute right-4 top-[36px]"
+          width="21"
+          height="18"
+          viewBox="0 0 21 18"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M11.366 17.5C10.9811 18.1667 10.0189 18.1667 9.63398 17.5L0.540709 1.75C0.155807 1.08333 0.636932 0.25 1.40673 0.25L19.5933 0.25C20.3631 0.25 20.8442 1.08333 20.4593 1.75L11.366 17.5Z"
+            fill="#e5e7eb"
+          />
+        </svg>
+      </div>
+      <div class="text-4xl flex flex-col font-bold text-[#676767] mt-8">
+        <span>Saurabh</span>
+        <span>Vishwakarma</span>
+      </div>
+      <div>
+        <span class="w-[50%] mt-8 flex flex-wrap font-semibold text-2xl">
+          I’m a 21-year-old Software Engineer from India, passionate about
+          technology and creating innovative solutions.
+        </span>
+      </div>
+      <div class="mt-8">
+        <a href="/">
+          <span class="font-bold">Let's Connect? </span>
+        </a>
+      </div>
+    </div>
+    <!-- image section -->
+    <div class=" flex items-center justify-center">
+      <img
+        src="/saurabh.jpg"
+        class="w-[600px] rounded-xl"
+        alt="saurabh_image"
+        srcset=""
+      />
+    </div>
+  </div>
+</div>
