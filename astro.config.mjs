19e1bf802bd5aeb4704de1c2f8e94f416b3952b2
Saurabh <75117608+Saurabh254@users.noreply.github.com>
2025-01-11 19:53:04 +0530

    fix: fixed about and project page content (#18)
    * fix: fixed about and project page content
    
    * updated: astro configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,15 @@
 import { defineConfig } from 'astro/config'
 
 import svelte from '@astrojs/svelte'
-
+import node from '@astrojs/node'
 import tailwind from '@astrojs/tailwind'
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'server', 
+  adapter: node({
+    mode: 'standalone',
+  }), 
   integrations: [
     svelte({ extensions: ['.svelte'] }),
     tailwind({
